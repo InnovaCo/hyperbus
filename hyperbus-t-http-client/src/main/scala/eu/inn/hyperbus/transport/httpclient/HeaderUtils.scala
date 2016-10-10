6f953d642b30b40749b68501609b7688efe5588a
Maga Abdurakhmanov <maqdev@gmail.com>
2016-10-11 00:59:19 +0200

    * working on http-client

--- hyperbus-t-http-client/src/main/scala/eu/inn/hyperbus/transport/httpclient/HeaderUtils.scala
+++ hyperbus-t-http-client/src/main/scala/eu/inn/hyperbus/transport/httpclient/HeaderUtils.scala
@@ -1,12 +1,23 @@
 package eu.inn.hyperbus.transport.httpclient
 
+import eu.inn.binders.naming.{CamelCaseToHyphenCaseConverter, HyphenCaseToCamelCaseConverter}
+
 object HeaderUtils {
-  val CONTENT_TYPE                = "Content-Type"
-  val CERTAIN_CONTENT_TYPE_START  = "application/vnd."
-  val CERTAIN_CONTENT_TYPE_END    = "+json"
-  val COMMON_CONTENT_TYPE         = "application/json"
-  val HYPERBUS_PREFIX = "Hyperbus-"
-  val HTTP_PREFIX = "Http-"
+  private val httpToHyperbusConverter = new HyphenCaseToCamelCaseConverter
+  private val hyperbusToHttpConverter = new CamelCaseToHyphenCaseConverter
+
+  private val CONTENT_TYPE                = "Content-Type"
+  private val CONTENT_TYPE_LC             = CONTENT_TYPE.toLowerCase
+  private val CONTENT_TYPE_HB             = httpToHyperbusConverter.convert(CONTENT_TYPE)
+  private val CERTAIN_CONTENT_TYPE_START  = "application/vnd."
+  private val CERTAIN_CONTENT_TYPE_END    = "+json"
+  private val COMMON_CONTENT_TYPE         = "application/json"
+  private val HYPERBUS_PREFIX             = "Hyperbus-"
+  private val HYPERBUS_PREFIX_LC          = HYPERBUS_PREFIX.toLowerCase
+  private val HTTP_PREFIX                 = "Http-"
+  private val HTTP_PREFIX_LC              = HTTP_PREFIX.toLowerCase
+  private val HTTP_PREFIX_HB              = "http"
+
 
   def httpContentTypeToGeneric(httpContentType: Option[String]): Option[String] = {
     httpContentType.map(_.toLowerCase) match {
@@ -34,9 +45,28 @@ object HeaderUtils {
   def httpToHyperbus(httpHeaders: Map[String, Seq[String]], exclude: Set[String] = Set.empty): Map[String, Seq[String]] = {
     httpHeaders.flatMap {
       case (k, _) if exclude.contains(k) ⇒ None
-      case (k, v)
+      case (k, v) if k.toLowerCase == CONTENT_TYPE_LC ⇒ Some( httpToHyperbusConverter.convert(k) →
+        v.flatMap(s ⇒ httpContentTypeToGeneric(Some(s)))
+      )
+      case (k, v) if k.toLowerCase.startsWith(HYPERBUS_PREFIX_LC)
+        && k.length > HYPERBUS_PREFIX_LC.length ⇒ Some(
+        httpToHyperbusConverter.convert(k.substring(HYPERBUS_PREFIX_LC.length)) → v
+      )
+      case (k, v) ⇒ Some(httpToHyperbusConverter.convert(HTTP_PREFIX + k) → v)
     }
   }
 
-  def hyperbusToHttp(hyperbusHeaders: Map[String, Seq[String]], exclude: Set[String] = Set.empty): Map[String, Seq[String]] = ???
+  def hyperbusToHttp(hyperbusHeaders: Map[String, Seq[String]], exclude: Set[String] = Set.empty): Map[String, Seq[String]] = {
+    hyperbusHeaders.flatMap {
+      case (k, _) if exclude.contains(k) ⇒ None
+      case (CONTENT_TYPE_HB, v) ⇒ Some( CONTENT_TYPE →
+        v.flatMap(s ⇒ genericContentTypeToHttp(Some(s)))
+      )
+      case (k, v) if k.startsWith(HTTP_PREFIX_HB)
+        && k.length > HTTP_PREFIX_HB.length ⇒ Some(
+        hyperbusToHttpConverter.convert(k.substring(HTTP_PREFIX_HB.length)) → v
+      )
+      case (k, v) ⇒ Some(HYPERBUS_PREFIX + hyperbusToHttpConverter.convert(k) → v)
+    }
+  }
 }
--- hyperbus-t-http-client/src/test/scala/HeaderUtilsTest.scala
+++ hyperbus-t-http-client/src/test/scala/HeaderUtilsTest.scala
@@ -1,7 +1,16 @@
-import org.scalatest.FlatSpec
+import eu.inn.hyperbus.transport.httpclient.HeaderUtils
+import org.scalatest.{FlatSpec, Matchers}
 
-class HeaderUtilsTest extends FlatSpec {
-  "A" should "B" in {
+class HeaderUtilsTest extends FlatSpec with Matchers {
+  "Http headers" should "have http prefix in hyperbus" in {
+    val http = Map("Accept-Language" → Seq("da, en-gb;q=0.8, en;q=0.7"))
+    val hb = HeaderUtils.httpToHyperbus(http)
+    hb shouldBe Map("httpAcceptLanguage" → Seq("da, en-gb;q=0.8, en;q=0.7"))
+  }
 
+  "Hyperbus headers" should "have Hyperbus- prefix when translated to http" in {
+    val hb = Map("messageId" → Seq("100500"))
+    val http = HeaderUtils.hyperbusToHttp(hb)
+    http shouldBe Map("Hyperbus-Message-Id" → Seq("100500"))
   }
 }
